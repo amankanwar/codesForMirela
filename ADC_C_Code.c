2c68b07245e7ca53b408d451134c9fda486d28c1
Aman Kanwar <iamamankanwar@gmail.com>
2020-05-10 01:06:33 +0530

    Update ADC_C_Code.c

--- ADC_C_Code.c
+++ ADC_C_Code.c
@@ -3,56 +3,55 @@
 #define ADCP	P1
 #define OUTP	P2	
 
-void sysInit();												// setting up system
-void storeData();								// reading the data and storing the same
-void captureADCdata();
+void sysInit();					// setting up system
+void storeData();				// reading the data and storing the same
+void captureADCdata();				// general ADC conversion
 
-sbit 		CS				=	P3^0;
-sbit 		RDpin		=	P3^1;
+sbit 		CS	=	P3^0;
+sbit 		RDpin	=	P3^1;
 sbit 		WRpin	=	P3^2;
-sbit 		INTR			=	P3^3;
+sbit 		INTR	=	P3^3;
 
 volatile unsigned short xdata ramAddr _at_ 0x4000;
 volatile unsigned short xdata externalAddr;
 
 
-void delay()
+void delay()					// Delay for 100 us
 {
-		char counter = 49;
-		for(; counter >0; counter--);
+	char counter = 49;
+	for(; counter >0; counter--);
 }
 
 
 int main(void)
 {
-	sysInit();
-	storeData();
-	
-	
-	while(1)
+	sysInit();				// calling for system initialization
+	storeData();				// storing 1000 ADC values
+
+	while(1)				// for general functionality
 	{
-			captureADCdata();	
+		captureADCdata();	
 	}		
-return  0;
+	return  0;
 }
 
 void sysInit()
 {
-	ADCP								=	0xFF;
-	externalAddr			=	ramAddr;
+	ADCP		=	0xFF;
+	externalAddr	=	ramAddr;
 }
 
 void storeData()
 {
 	short int counter = 1000;
 	
-	for(counter;	counter>0;	counter--)		// this will run 1000 times
+	for(counter; counter>0; counter--)	// this will run 1000 times
 	{
-			captureADCdata();
-			externalAddr	=	ADCP;
-			++externalAddr;						// incrementing the externalAddr
-			
-			delay();						// 100 us delay
+		captureADCdata();
+		externalAddr	=	ADCP;
+		++externalAddr;			// incrementing the externalAddr
+		
+		delay();			// 100 us delay
 	}
 
 }
@@ -61,8 +60,8 @@ void captureADCdata()
 {
 		//----------resetting pins--------
 		RDpin		=	1;
-		WRpin	=	1;
-		INTR			=	1;
+		WRpin		=	1;
+		INTR		=	1;
 		//-------------------------------------
 		
 		//---------converting the data Low-To-High Pulse----
@@ -71,9 +70,8 @@ void captureADCdata()
 		//---------------------------------------------------------------------
 	
 		//--------Motinoring the INTR pin	--------------------------
-		while(INTR);
+		while(INTR);			// stay here till INTR is HIGH
 		
-
 		//---------reading the data High-To-Low Pulse----
 		RDpin		=	1;
 		RDpin		=	0;
